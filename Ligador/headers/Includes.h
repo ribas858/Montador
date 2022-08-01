dd466937b3c914688e3bb3e1dc4dc7c61d587058
ribas858 <ribas858@gmail.com>
2022-08-01 20:21:03 -0300

    save

--- Ligador/headers/Includes.h
+++ Ligador/headers/Includes.h
@@ -10,18 +10,11 @@
 #include <cctype>
 #include <vector>
 
-#include "Simbolo.h"
 #include "Instrucao.h"
-#include "Diretiva.h"
-#include "DiretivaExtend.h"
 #include "Definicao.h"
 #include "Uso.h"
-#include "Plus.h"
-#include "Enunciado.h"
 
 using namespace std;
 using std::transform; using std::toupper;
-
-
 
 #endif
